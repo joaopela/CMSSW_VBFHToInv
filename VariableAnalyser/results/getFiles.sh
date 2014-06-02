293a0d1ef3b948d9b4391858d77a3a38022b9158
Joao Pela <pela@cern.ch>
2014-06-02 12:44:48 +0200

    Adding file to merge CRAB results

---
+++ VariableAnalyser/results/getFiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+hadd Powheg-Htoinv-mH125.root ../crab/AnalysisWithPv/Powheg-Htoinv-mH125/res/out_*.root
+hadd QCD-Pt-120to170_VBF-MET40.root ../crab/QCD-Pt-120to170_VBF-MET40/res/out_*.root
+hadd QCD-Pt-170to300_VBF-MET40.root ../crab/QCD-Pt-170to300_VBF-MET40/res/out_*.root
+hadd QCD-Pt-300to470_VBF-MET40.root ../crab/QCD-Pt-300to470_VBF-MET40/res/out_*.root
+hadd QCD-Pt-470to600_VBF-MET40.root ../crab/QCD-Pt-470to600_VBF-MET40/res/out_*.root
+hadd QCD-Pt-80to120_VBF-MET40.root ../crab/QCD-Pt-80to120_VBF-MET40/res/out_*.root
