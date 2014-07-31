4c70684b817f4c6db1a23071b4d9171e3c97a1cf
Joao Pela <pela@cern.ch>
2014-07-31 19:27:50 +0100

    Adding input for output filename and additional AlgoTester inputs. Setting the output of event number to 1000.

--- TriggerStudies/test/TrigStudies_cfg.py
+++ TriggerStudies/test/TrigStudies_cfg.py
@@ -4,7 +4,7 @@ process = cms.Process("TrgEff")
 
 ################################################################
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = 100
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool( True )
@@ -86,18 +86,24 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.trgEff = cms.EDAnalyzer('TrigStudies',
                               
   verbose                    = cms.untracked.bool(False),
-  inputTag_L1GTReadoutRecord = cms.InputTag("gtDigis"),
-  
-  inputTag_HLTResults        = cms.InputTag("TriggerResults::HLT"),
-  selHLTrigger               = cms.vstring("HLT_DiPFJet40_PFMETnoMu65_MJJ800VBF_AllJets_v",
-                                           "HLT_DiPFJet40_PFMETnoMu65_MJJ600VBF_LeadingJets_v",
-                                           "HLT_DiPFJet40_PFMETnoMu75_MJJ800VBF_AllJets_v",
-                                           "HLT_DiPFJet40_PFMETnoMu75_MJJ600VBF_LeadingJets_v",                                          
-                                           "HLT_DiJet20_MJJ650_AllJets_DEta3p5_HT120_VBF_v",
-                                           "HLT_DiJet30_MJJ700_AllJets_DEta3p5_VBF_v",
-                                           "HLT_DiJet35_MJJ650_AllJets_DEta3p5_VBF_v",
-                                           "HLT_DiJet35_MJJ700_AllJets_DEta3p5_VBF_v",
-                                           "HLT_DiJet35_MJJ750_AllJets_DEta3p5_VBF_v"),
+  inputTag_L1GTReadoutRecord = cms.untracked.InputTag("gtDigis"),
+  inputTag_HLTResults        = cms.untracked.InputTag("TriggerResults::HLT"),
+
+  outputFilename = cms.untracked.string("TrigStudiesResults.root"),
+
+  selHLTrigger    = cms.vstring("HLT_DiPFJet40_PFMETnoMu65_MJJ800VBF_AllJets_v",
+                                "HLT_DiPFJet40_PFMETnoMu65_MJJ600VBF_LeadingJets_v",
+                                "HLT_DiPFJet40_PFMETnoMu75_MJJ800VBF_AllJets_v",
+                                "HLT_DiPFJet40_PFMETnoMu75_MJJ600VBF_LeadingJets_v",                                          
+                                "HLT_DiJet20_MJJ650_AllJets_DEta3p5_HT120_VBF_v",
+                                "HLT_DiJet30_MJJ700_AllJets_DEta3p5_VBF_v",
+                                "HLT_DiJet35_MJJ650_AllJets_DEta3p5_VBF_v",
+                                "HLT_DiJet35_MJJ700_AllJets_DEta3p5_VBF_v",
+                                "HLT_DiJet35_MJJ750_AllJets_DEta3p5_VBF_v"),
+
+  runAlgoTester   = cms.untracked.bool(True),
+  algoTesterAlgos = cms.vstring("L1_DoubleJetC60_ETM60",
+                                "L1_DoubleJet60_ETM60"),
   
 )
 
