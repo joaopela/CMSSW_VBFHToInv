e294b0974178b760d8e33a55d3c84e6eb7ef4364
Joao Pela <pela@cern.ch>
2014-10-14 19:27:55 +0100

    Addapting for new produced samples

--- ReRunHLT/test/runPatTrigger_localRun_PU20bx25_VBFHInv_cfg.py
+++ ReRunHLT/test/runPatTrigger_localRun_PU20bx25_VBFHInv_cfg.py
@@ -4,7 +4,7 @@ process = cms.Process('PAT')
 
 ################################################################
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = 10
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool( True )
@@ -21,16 +21,27 @@ process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 # Input source
-process.load("VBFHiggsToInvisible.Samples.VBF_HToInv_M-125_13TeV_powheg-pythia6_pela-TrigStudies_Run2015_Legacy_ReRunHLT_v3_PU20bx25_VBF_HToInv_M-125_13TeV_powheg-pythia6_USER_cfi")
-
-process.maxEvents = cms.untracked.PSet(
-  input = cms.untracked.int32(5)
+process.source = cms.Source("PoolSource",
+  fileNames = cms.untracked.vstring(
+    '/store/user/pela/VBF_HToInv_M-125_13TeV_powheg-pythia6/TrigStudies_Run2015_L1EmulatorStage1_HLT_v2_PU40bx25_VBF_HToInv_M-125_13TeV_powheg-pythia6/740729526d0c0f1eb16eda620a6cff9a/outputA_10_1_RPA.root',
+  ),
 )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.hltPathStudies = cms.EDAnalyzer('HLTPathStudies',
-                              
+
   verbose                    = cms.untracked.bool(True),  
-  inputTag_HLTResults        = cms.untracked.InputTag("TriggerResults::HLT2"),
+  inputTag_HLTResults        = cms.untracked.InputTag("TriggerResults::TEST"),
+
+  inputTag_L1EmParticle_Isolated    = cms.untracked.InputTag("l1ExtraLayer2","Isolated"   ),
+  inputTag_L1EmParticle_NonIsolated = cms.untracked.InputTag("l1ExtraLayer2","NonIsolated"),
+  inputTag_L1EtMissParticle_MET     = cms.untracked.InputTag("l1ExtraLayer2","MET"        ),
+  inputTag_L1EtMissParticle_MHT     = cms.untracked.InputTag("l1ExtraLayer2","MHT"        ),
+  inputTag_L1HFRings                = cms.untracked.InputTag("l1ExtraLayer2",""           ),
+  inputTag_L1JetParticle_Central    = cms.untracked.InputTag("l1ExtraLayer2","Central"    ),
+  inputTag_L1JetParticle_Forward    = cms.untracked.InputTag("l1ExtraLayer2","Forward"    ),
+  inputTag_L1JetParticle_Tau        = cms.untracked.InputTag("l1ExtraLayer2","Tau"        ),
+  inputTag_L1MuonParticle           = cms.untracked.InputTag("l1ExtraLayer2",""           ),
 
 )
 
@@ -54,7 +65,7 @@ process.GlobalTag = GlobalTag(process.GlobalTag, 'POSTLS162_V2::All', '')
 process.p = cms.Path()
 
 from PhysicsTools.PatAlgos.tools.trigTools import *
-switchOnTrigger( process, path = 'p', hltProcess = 'HLT2',outputModule = 'out')
+switchOnTrigger( process, path = 'p', hltProcess = 'TEST',outputModule = 'out')
 #switchOnTrigger( process, hltProcess = 'HLT2',outputModule = 'out')
 
 process.p += process.hltPathStudies
@@ -65,4 +76,4 @@ process.p += process.hltPathStudies
 #process.patTrigger.hltProcess = cms.untracked.string('HLT2')
 #switchOnTrigger( process )                       
     
-process.e = cms.EndPath(process.out)
+#process.e = cms.EndPath(process.out)
