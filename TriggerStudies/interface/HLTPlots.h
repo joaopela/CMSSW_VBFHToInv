c2e4a82a342349d31898e5259f15c7a4f03438af
Joao Pela <pela@cern.ch>
2014-10-20 23:33:15 +0100

    Simplified plot making and added some more plots for calo and pt object,

--- TriggerStudies/interface/HLTPlots.h
+++ TriggerStudies/interface/HLTPlots.h
@@ -2,22 +2,95 @@
 #ifndef VBFHiggsToInvisible_TriggerStudies_HLTPlots_H
 #define VBFHiggsToInvisible_TriggerStudies_HLTPlots_H
 
+// CMSSW includes
+#include "VBFHiggsToInvisible/TriggerStudies/interface/HLTEventData.h"
+
 // ROOT includes
 #include "TH1D.h"
 
 // System include files
 #include <string>
 #include <utility>
+#include <iostream>
 
 class HLTPlotsData{
 public:
   
   HLTPlotsData(){
-    met.first = false;
+    this->init();
+  }
+
+  HLTPlotsData(HLTEventData* data){
+
+    this->init();
+    
+    // Getting variables from loose pf trigger
+    if(data->getPathFired("HLT_PFMET_PFVBF_v1")){
+
+      HLTPathData* thePath = data->getPathData("HLT_PFMET_PFVBF_v1");
+
+      pf_met.first  = true;
+      pf_met.second = thePath->getFilterObjects("hltPFMET80Filter").at(0)->pt();
+
+      std::vector<HLTDiobject*> myDijet = thePath->getFilterDiobjects("hltDiPFJet10");
+      if(myDijet.size()>0){
+        pf_dijet_maxDEta.first  = true;
+        pf_dijet_maxDEta.second = myDijet[0]->deta();
+        pf_dijet_maxMjj.first   = true;
+        pf_dijet_maxMjj.second  = myDijet[0]->mjj();
+      }
+
+      for(unsigned i=1; i<myDijet.size(); i++){
+        
+        HLTDiobject* dijet = myDijet[i];
+        if(dijet->deta()>pf_dijet_maxDEta.second){pf_dijet_maxDEta.second=dijet->deta();}
+        if(dijet->mjj() >pf_dijet_maxMjj.second) {pf_dijet_maxMjj .second=dijet->mjj();}
+      }
+    }
+    
+    // Getting variables from loose calo trigger
+    if(data->getPathFired("HLT_CaloMET_CaloVBF_v1")){
+
+      HLTPathData* thePath = data->getPathData("HLT_CaloMET_CaloVBF_v1");
+ 
+      calo_met.first  = true; 
+      calo_met.second = thePath->getFilterObjects("hltMET65").at(0)->pt();
+
+      std::vector<HLTDiobject*> myDijet = thePath->getFilterDiobjects("hltDiCaloJet10");
+      if(myDijet.size()>0){
+        calo_dijet_maxDEta.first  = true;
+        calo_dijet_maxDEta.second = myDijet[0]->deta();
+        calo_dijet_maxMjj.first   = true;
+        calo_dijet_maxMjj.second  = myDijet[0]->mjj();
+      }
+
+      for(unsigned i=1; i<myDijet.size(); i++){
+        
+        HLTDiobject* dijet = myDijet[i];
+        if(dijet->deta()>calo_dijet_maxDEta.second){calo_dijet_maxDEta.second=dijet->deta();}
+        if(dijet->mjj() >calo_dijet_maxMjj.second) {calo_dijet_maxMjj .second=dijet->mjj();}
+      }
+    }
   }
   
-  std::pair<bool,double> met;
-
+  void init(){
+    pf_met.first           = false;
+    pf_dijet_maxDEta.first = false;
+    pf_dijet_maxMjj.first  = false;
+    
+    calo_met.first            = false;
+    calo_dijet_maxDEta.first  = false;
+    calo_dijet_maxMjj.first   = false;
+  }
+  
+  std::pair<bool,double> pf_met;
+  std::pair<bool,double> pf_dijet_maxDEta;
+  std::pair<bool,double> pf_dijet_maxMjj;
+  
+  std::pair<bool,double> calo_met;
+  std::pair<bool,double> calo_dijet_maxDEta;
+  std::pair<bool,double> calo_dijet_maxMjj;
+  
 };
 
 class HLTPlots{
@@ -31,8 +104,13 @@ public:
   void fill(HLTPlotsData data);
   
   // Class member objects  
-  TH1D *met;
-
+  TH1D *pf_met;
+  TH1D *pf_dijet_maxDEta;
+  TH1D *pf_dijet_maxMjj;
+  
+  TH1D *calo_met;
+  TH1D *calo_dijet_maxDEta;
+  TH1D *calo_dijet_maxMjj;
   
 };
 
--- TriggerStudies/src/HLTPlots.cc
+++ TriggerStudies/src/HLTPlots.cc
@@ -7,25 +7,70 @@ using namespace std;
 
 HLTPlots::HLTPlots(){
 
-  met = 0;
+  pf_met           = 0;
+  pf_dijet_maxDEta = 0;
+  pf_dijet_maxMjj  = 0;
   
+  calo_met           = 0;
+  calo_dijet_maxDEta = 0;
+  calo_dijet_maxMjj  = 0;
+
 }
 
 HLTPlots::HLTPlots(TDirectory* d){
+
+  // PF plots
+  pf_met = new TH1D("pf_met","HLT PF MET"  , 500,  0,500);
+  pf_met->GetXaxis()->SetTitle("MET [GeV]");
+  pf_met->GetYaxis()->SetTitle("Entries");
+  pf_met->SetDirectory(d);
   
-  met = new TH1D("hlt_etm","HLT ETM"  , 500,-0.5,495.5);
-  met->GetXaxis()->SetTitle("HLT ETM [GeV]");
-  met->GetYaxis()->SetTitle("Entries");
-  met->SetDirectory(d);
+  pf_dijet_maxDEta = new TH1D("pf_dijet_maxDEta","HLT PF Dijet max(#Delta#eta)" ,100,0,10);
+  pf_dijet_maxDEta->GetXaxis()->SetTitle("Dijet max(#Delta#eta)");
+  pf_dijet_maxDEta->GetYaxis()->SetTitle("Entries");
+  pf_dijet_maxDEta->SetDirectory(d);
+  
+  pf_dijet_maxMjj = new TH1D("pf_dijet_maxMjj","HLT PF Dijet max(m_{jj})"  , 400,  0,2000);
+  pf_dijet_maxMjj->GetXaxis()->SetTitle("Dijet max(m_{jj} [GeV]");
+  pf_dijet_maxMjj->GetYaxis()->SetTitle("Entries");
+  pf_dijet_maxMjj->SetDirectory(d);
+  
+  // Calo plots
+  calo_met = new TH1D("calo_met","HLT PF MET"  , 500, 0,500);
+  calo_met->GetXaxis()->SetTitle("HLT MET [GeV]");
+  calo_met->GetYaxis()->SetTitle("Entries");
+  calo_met->SetDirectory(d);
+  
+  calo_dijet_maxDEta = new TH1D("calo_dijet_maxDEta","HLT PF Dijet max(#Delta#eta)" ,100,0,10);
+  calo_dijet_maxDEta->GetXaxis()->SetTitle("Dijet max(#Delta#eta)");
+  calo_dijet_maxDEta->GetYaxis()->SetTitle("Entries");
+  calo_dijet_maxDEta->SetDirectory(d);
+  
+  calo_dijet_maxMjj = new TH1D("calo_dijet_maxMjj","HLT PF Dijet max(m_{jj})"  , 400, 0,2000);
+  calo_dijet_maxMjj->GetXaxis()->SetTitle("Dijet max(m_{jj} [GeV]");
+  calo_dijet_maxMjj->GetYaxis()->SetTitle("Entries");
+  calo_dijet_maxMjj->SetDirectory(d);
   
 }
 
 void HLTPlots::fill(HLTPlotsData data){
   
-  if(data.met.first){met->Fill(data.met.second);}
-
+  if(data.pf_met          .first){pf_met          ->Fill(data.pf_met          .second);}
+  if(data.pf_dijet_maxDEta.first){pf_dijet_maxDEta->Fill(data.pf_dijet_maxDEta.second);}
+  if(data.pf_dijet_maxMjj .first){pf_dijet_maxMjj ->Fill(data.pf_dijet_maxMjj .second);}
+  
+  if(data.calo_met          .first){calo_met          ->Fill(data.calo_met          .second);}
+  if(data.calo_dijet_maxDEta.first){calo_dijet_maxDEta->Fill(data.calo_dijet_maxDEta.second);}
+  if(data.calo_dijet_maxMjj .first){calo_dijet_maxMjj ->Fill(data.calo_dijet_maxMjj .second);}
+  
 }
 
 HLTPlots::~HLTPlots(){
-  delete met;
+  delete pf_met;
+  delete pf_dijet_maxDEta;
+  delete pf_dijet_maxMjj;
+  
+  delete calo_met;
+  delete calo_dijet_maxDEta;
+  delete calo_dijet_maxMjj;
 }
