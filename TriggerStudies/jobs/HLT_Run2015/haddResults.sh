f8772d02ce2e0db2f97ed449aa4a6374ea90b8e8
Joao Pela <pela@cern.ch>
2014-10-22 15:06:11 +0100

    First version of code to hadd results of all batch jobs for HLT analysis

---
+++ TriggerStudies/jobs/HLT_Run2015/haddResults.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+hadd PU40bx25_VBFInv.root PU40bx25_VBFInv/HLTPathStudiesResults*.root
+hadd PU40bx25_NeutrinoGun.root PU40bx25_NeutrinoGun/HLTPathStudiesResults*.root
+hadd PU40bx25_QCD_Pt-1000to1400.root PU40bx25_QCD_Pt-1000to140/HLTPathStudiesResults*.root
+hadd PU40bx25_QCD_Pt-120to170.root PU40bx25_QCD_Pt-120to170/HLTPathStudiesResults*.root
+hadd PU40bx25_QCD_Pt-1400to1800.root PU40bx25_QCD_Pt-1400to1800/HLTPathStudiesResults*.root
+hadd PU40bx25_QCD_Pt-170to300.root PU40bx25_QCD_Pt-170to300/HLTPathStudiesResults*.root
+hadd PU40bx25_QCD_Pt-300to470.root PU40bx25_QCD_Pt-300to470/HLTPathStudiesResults*.root
+hadd PU40bx25_QCD_Pt-30to50.root PU40bx25_QCD_Pt-30to50/HLTPathStudiesResults*.root
+hadd PU40bx25_QCD_Pt-470to600.root PU40bx25_QCD_Pt-470to600/HLTPathStudiesResults*.root
+hadd PU40bx25_QCD_Pt-50to80.root PU40bx25_QCD_Pt-50to80/HLTPathStudiesResults*.root
+hadd PU40bx25_QCD_Pt-600to800.root PU40bx25_QCD_Pt-600to800/HLTPathStudiesResults*.root
+hadd PU40bx25_QCD_Pt-800to1000.root PU40bx25_QCD_Pt-800to1000/HLTPathStudiesResults*.root
+
