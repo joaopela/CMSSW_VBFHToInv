dc2919f153fbe80efd025764eb3ea43423d133e6
Joao Pela <pela@cern.ch>
2015-04-10 16:55:04 +0200

    Scripts to run production

---
+++ SampleProduction/test/13TeV/VBFQCD/runQCD_Pt-30to50.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+
+source  /afs/cern.ch/cms/cmsset_default.sh
+export SCRAM_ARCH=slc6_amd64_gcc481
+cd /afs/cern.ch/user/p/pela/work/cms/vbfinv/slc6/prodTest1/CMSSW_7_2_0_patch1/src/VBFHiggsToInvisible/SampleProduction/test/13TeV/VBFQCD
+eval `scram runtime -sh`
+
+cat /proc/cpuinfo
+cat /proc/meminfo
+
+cmsRun QCD_Pt-30to50_step1_cfg.py | tee QCD_Pt-30to50_step1.log
+cmsRun QCD_Pt-30to50_step2_cfg.py | tee QCD_Pt-30to50_step2.log
