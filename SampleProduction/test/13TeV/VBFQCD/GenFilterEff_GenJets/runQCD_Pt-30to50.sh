f45d2ba7ae9dcbd9dfce9f068b5b9c7897fcbf93
Joao Pela <pela@cern.ch>
2015-04-16 17:39:14 +0200

    Driving code for jobs

---
+++ SampleProduction/test/13TeV/VBFQCD/GenFilterEff_GenJets/runQCD_Pt-30to50.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+
+source  /afs/cern.ch/cms/cmsset_default.sh
+export SCRAM_ARCH=slc6_amd64_gcc481
+cd /afs/cern.ch/user/p/pela/work/cms/vbfinv/slc6/prodTest1/CMSSW_7_2_0_patch1/src/VBFHiggsToInvisible/SampleProduction/test/13TeV/VBFQCD/GenFilterEff_GenJets/
+eval `scram runtime -sh`
+
+cat /proc/cpuinfo
+cat /proc/meminfo
+
+cmsRun QCD_Pt-30to50_Ev100k_cfg.py | tee QCD_Pt-30to50.log
+
