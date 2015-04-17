5668a9a73788d13e2e6444008dbb5d371d2fbf25
Joao Pela <pela@cern.ch>
2015-04-17 16:31:53 +0200

    Adding new test point

---
+++ SampleProduction/test/13TeV/VBFQCD/GenFilterEff_Both/Pt20_Eta5p0_dEta3p2_Mjj700_MET40/run170to300.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+
+source  /afs/cern.ch/cms/cmsset_default.sh
+export SCRAM_ARCH=slc6_amd64_gcc481
+cd /afs/cern.ch/user/p/pela/work/cms/vbfinv/slc6/prodTest1/CMSSW_7_2_0_patch1/src/
+eval `scram runtime -sh`
+
+cd VBFHiggsToInvisible/SampleProduction/test/13TeV/VBFQCD/GenFilterEff_Both/Pt20_Eta5p0_dEta3p2_Mjj700_MET40/
+
+CMSSW_JOB_CFG="QCD_Pt-170to300_step1_cfg"
+
+cat /proc/cpuinfo &> $CMSSW_JOB_CFG.cpu.log
+cat /proc/meminfo &> $CMSSW_JOB_CFG.mem.log
+
+# Making job status
+touch $CMSSW_JOB_CFG.status.running
+
+cmsRun $CMSSW_JOB_CFG.py | tee $CMSSW_JOB_CFG.job.log
+
+# Updating job status
+rm $CMSSW_JOB_CFG.job.status.running
+touch $CMSSW_JOB_CFG.job.status.done
