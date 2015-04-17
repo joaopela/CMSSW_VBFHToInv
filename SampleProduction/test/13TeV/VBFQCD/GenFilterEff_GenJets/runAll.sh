e6b8b5ff7a84f4bb6ea675d0d71a9b44f8984794
Joao Pela <pela@cern.ch>
2015-04-17 19:37:55 +0200

    Adding script to submit all test points.

---
+++ SampleProduction/test/13TeV/VBFQCD/GenFilterEff_GenJets/runAll.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd Pt40_Eta4p75_DEta3p5_Dphi1p5_Mjj1000/; ./submitQCD.sh; cd -
+cd Pt40_Eta4p75_DEta3p5_Dphi1p5_Mjj600/; ./submitQCD.sh; cd -
+cd Pt40_Eta4p75_DEta3p5_Mjj600/; ./submitQCD.sh; cd -
+cd Pt50_Eta4p75_DEta3p5_Dphi1p5_Mjj1000/; ./submitQCD.sh; cd -
+cd Pt50_Eta4p75_DEta3p5_Dphi2p0_Mjj1000/; ./submitQCD.sh; cd -
+cd Pt50_Eta4p75_DEta3p5_Dphi2p5_Mjj1000/; ./submitQCD.sh; cd -
+cd Pt50_Eta4p75_DEta3p5_Mjj1000/; ./submitQCD.sh; cd -
