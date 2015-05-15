798c9e0011426367f67e2b6db895f001fe9bbf3d
Joao Pela <pela@cern.ch>
2015-05-15 16:28:52 +0200

    Changing names

--- SampleProduction/test/13TeV/VBFQCD/OfflineFilterEff/haddJobs.sh
+++ SampleProduction/test/13TeV/VBFQCD/OfflineFilterEff/haddJobs.sh
@@ -3,11 +3,11 @@
 # This script will add all the results from the jobs produced with the script 'createJobs.sh'
 # this will be done with ROOT hadd command
 
-hadd GenFilterAnalyzer_QCD_Pt-30to50.root   QCD_Pt-30to50/GenFilterAnalyzer_*.root
-hadd GenFilterAnalyzer_QCD_Pt-50to80.root   QCD_Pt-50to80/GenFilterAnalyzer_*.root
-hadd GenFilterAnalyzer_QCD_Pt-80to120.root  QCD_Pt-80to120/GenFilterAnalyzer_*.root
-hadd GenFilterAnalyzer_QCD_Pt-120to170.root QCD_Pt-120to170/GenFilterAnalyzer_*.root
-hadd GenFilterAnalyzer_QCD_Pt-170to300.root QCD_Pt-170to300/GenFilterAnalyzer_*.root
-hadd GenFilterAnalyzer_QCD_Pt-300to470.root QCD_Pt-300to470/GenFilterAnalyzer_*.root
-hadd GenFilterAnalyzer_QCD_Pt-470to600.root QCD_Pt-470to600/GenFilterAnalyzer_*.root
-hadd GenFilterAnalyzer_QCD_Pt-600to800.root QCD_Pt-600to800/GenFilterAnalyzer_*.root
+hadd OfflineFilterAnalyzer_QCD_Pt-30to50.root   QCD_Pt-30to50/OfflineFilterAnalyzer_*.root
+hadd OfflineFilterAnalyzer_QCD_Pt-50to80.root   QCD_Pt-50to80/OfflineFilterAnalyzer_*.root
+hadd OfflineFilterAnalyzer_QCD_Pt-80to120.root  QCD_Pt-80to120/OfflineFilterAnalyzer_*.root
+hadd OfflineFilterAnalyzer_QCD_Pt-120to170.root QCD_Pt-120to170/OfflineFilterAnalyzer_*.root
+hadd OfflineFilterAnalyzer_QCD_Pt-170to300.root QCD_Pt-170to300/OfflineFilterAnalyzer_*.root
+hadd OfflineFilterAnalyzer_QCD_Pt-300to470.root QCD_Pt-300to470/OfflineFilterAnalyzer_*.root
+hadd OfflineFilterAnalyzer_QCD_Pt-470to600.root QCD_Pt-470to600/OfflineFilterAnalyzer_*.root
+hadd OfflineFilterAnalyzer_QCD_Pt-600to800.root QCD_Pt-600to800/OfflineFilterAnalyzer_*.root
