15f836337eaaba1942f4e019886761b5d63d23f6
Joao Pela <pela@cern.ch>
2015-06-01 12:27:36 +0200

    First version of script to extract results

---
+++ Study_RunII_SampleProd_QCD-VBF-MET/test/Unfiltered/results_cfg.py
@@ -0,0 +1,16 @@
+results = dict();
+
+myOrder  = []
+myOrder.append('30-50')
+myOrder.append('50-80')
+myOrder.append('80-120')
+myOrder.append('120-170')
+myOrder.append('300-470')
+myOrder.append('470-600')
+
+results['30-50']   = 'LSFJOB_656414827'
+results['50-80']   = 'LSFJOB_656414829'
+results['80-120']  = 'LSFJOB_656414830'
+results['120-170'] = 'LSFJOB_656414831'
+results['300-470'] = 'LSFJOB_656414833'
+results['470-600'] = 'LSFJOB_656440203'
