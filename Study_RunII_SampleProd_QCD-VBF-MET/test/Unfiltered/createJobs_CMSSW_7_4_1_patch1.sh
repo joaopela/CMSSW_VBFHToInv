c6d920c9f3c18e27fb7e4fa20680b6ba3ad64ef9
Joao Pela <pela@cern.ch>
2015-06-03 21:55:00 +0200

    Now create jobs for both step 1 and 2

--- Study_RunII_SampleProd_QCD-VBF-MET/test/Unfiltered/createJobs_CMSSW_7_4_1_patch1.sh
+++ Study_RunII_SampleProd_QCD-VBF-MET/test/Unfiltered/createJobs_CMSSW_7_4_1_patch1.sh
@@ -1,10 +1,23 @@
 #!/bin/bash 
 
-hepfwMakeBatchJobs -jobType cmssw -jobName QCD_30to50   -jobQueue 2nd -cmsswCfg QCD_Pt-30to50_CMSSW_7_4_1_patch1_step1_cfg.py   -nJobs 1 -jobNEvents 100
-hepfwMakeBatchJobs -jobType cmssw -jobName QCD_50to80   -jobQueue 2nd -cmsswCfg QCD_Pt-50to80_CMSSW_7_4_1_patch1_step1_cfg.py   -nJobs 1 -jobNEvents 100
-hepfwMakeBatchJobs -jobType cmssw -jobName QCD_80to120  -jobQueue 2nd -cmsswCfg QCD_Pt-80to120_CMSSW_7_4_1_patch1_step1_cfg.py  -nJobs 1 -jobNEvents 100
-hepfwMakeBatchJobs -jobType cmssw -jobName QCD_120to170 -jobQueue 2nd -cmsswCfg QCD_Pt-120to170_step1_CMSSW_7_4_1_patch1_cfg.py -nJobs 1 -jobNEvents 100
-hepfwMakeBatchJobs -jobType cmssw -jobName QCD_170to300 -jobQueue 2nd -cmsswCfg QCD_Pt-170to300_step1_CMSSW_7_4_1_patch1_cfg.py -nJobs 1 -jobNEvents 100
-hepfwMakeBatchJobs -jobType cmssw -jobName QCD_300to470 -jobQueue 2nd -cmsswCfg QCD_Pt-300to470_step1_CMSSW_7_4_1_patch1_cfg.py -nJobs 1 -jobNEvents 100
-hepfwMakeBatchJobs -jobType cmssw -jobName QCD_470to600 -jobQueue 2nd -cmsswCfg QCD_Pt-470to600_step1_CMSSW_7_4_1_patch1_cfg.py -nJobs 1 -jobNEvents 100
-hepfwMakeBatchJobs -jobType cmssw -jobName QCD_600to800 -jobQueue 2nd -cmsswCfg QCD_Pt-600to800_step1_CMSSW_7_4_1_patch1_cfg.py -nJobs 1 -jobNEvents 100
+hepfwMakeBatchJobs -jobType cmssw -jobName QCD_Pt-30to50_step1   -jobQueue 2nd -cmsswCfg QCD_Pt-30to50_CMSSW_7_4_1_patch1_step1_cfg.py   -nJobs 1 -jobNEvents 100
+hepfwMakeBatchJobs -jobType cmssw -jobName QCD_Pt-50to80_step1   -jobQueue 2nd -cmsswCfg QCD_Pt-50to80_CMSSW_7_4_1_patch1_step1_cfg.py   -nJobs 1 -jobNEvents 100
+hepfwMakeBatchJobs -jobType cmssw -jobName QCD_Pt-80to120_step1  -jobQueue 2nd -cmsswCfg QCD_Pt-80to120_CMSSW_7_4_1_patch1_step1_cfg.py  -nJobs 1 -jobNEvents 100
+hepfwMakeBatchJobs -jobType cmssw -jobName QCD_Pt-120to170_step1 -jobQueue 2nd -cmsswCfg QCD_Pt-120to170_step1_CMSSW_7_4_1_patch1_cfg.py -nJobs 1 -jobNEvents 100
+hepfwMakeBatchJobs -jobType cmssw -jobName QCD_Pt-170to300_step1 -jobQueue 2nd -cmsswCfg QCD_Pt-170to300_step1_CMSSW_7_4_1_patch1_cfg.py -nJobs 1 -jobNEvents 100
+hepfwMakeBatchJobs -jobType cmssw -jobName QCD_Pt-300to470_step1 -jobQueue 2nd -cmsswCfg QCD_Pt-300to470_step1_CMSSW_7_4_1_patch1_cfg.py -nJobs 1 -jobNEvents 100
+hepfwMakeBatchJobs -jobType cmssw -jobName QCD_Pt-470to600_step1 -jobQueue 2nd -cmsswCfg QCD_Pt-470to600_step1_CMSSW_7_4_1_patch1_cfg.py -nJobs 1 -jobNEvents 100
+hepfwMakeBatchJobs -jobType cmssw -jobName QCD_Pt-600to800_step1 -jobQueue 2nd -cmsswCfg QCD_Pt-600to800_step1_CMSSW_7_4_1_patch1_cfg.py -nJobs 1 -jobNEvents 100
+
+hepfwMakeBatchJobs -jobType cmssw -jobName QCD_Pt-30to50_step2   -jobQueue 8nh -cmsswCfg QCD_Pt-30to50_step2_CMSSW_7_4_1_patch1_cfg.py   -nJobs 1 -jobNEvents 100
+hepfwMakeBatchJobs -jobType cmssw -jobName QCD_Pt-50to80_step2   -jobQueue 8nh -cmsswCfg QCD_Pt-50to80_step2_CMSSW_7_4_1_patch1_cfg.py   -nJobs 1 -jobNEvents 100
+hepfwMakeBatchJobs -jobType cmssw -jobName QCD_Pt-80to120_step2  -jobQueue 8nh -cmsswCfg QCD_Pt-80to120_step2_CMSSW_7_4_1_patch1_cfg.py  -nJobs 1 -jobNEvents 100
+hepfwMakeBatchJobs -jobType cmssw -jobName QCD_Pt-120to170_step2 -jobQueue 8nh -cmsswCfg QCD_Pt-120to170_step2_CMSSW_7_4_1_patch1_cfg.py -nJobs 1 -jobNEvents 100
+hepfwMakeBatchJobs -jobType cmssw -jobName QCD_Pt-170to300_step2 -jobQueue 8nh -cmsswCfg QCD_Pt-170to300_step2_CMSSW_7_4_1_patch1_cfg.py -nJobs 1 -jobNEvents 100
+hepfwMakeBatchJobs -jobType cmssw -jobName QCD_Pt-300to470_step2 -jobQueue 8nh -cmsswCfg QCD_Pt-300to470_step2_CMSSW_7_4_1_patch1_cfg.py -nJobs 1 -jobNEvents 100
+hepfwMakeBatchJobs -jobType cmssw -jobName QCD_Pt-470to600_step2 -jobQueue 8nh -cmsswCfg QCD_Pt-470to600_step2_CMSSW_7_4_1_patch1_cfg.py -nJobs 1 -jobNEvents 100
+hepfwMakeBatchJobs -jobType cmssw -jobName QCD_Pt-600to800_step2 -jobQueue 8nh -cmsswCfg QCD_Pt-600to800_step2_CMSSW_7_4_1_patch1_cfg.py -nJobs 1 -jobNEvents 100
+
+
+
+
