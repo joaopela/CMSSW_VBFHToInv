b88408ed611640ad7c831d375e8379d98c8ba3e1
Joao Pela <pela@cern.ch>
2015-06-03 21:20:51 +0200

    Changing name and updating step 1 results directory

---
+++ Study_RunII_SampleProd_QCD-VBF-MET/test/Unfiltered/results_step1_cfg.py
@@ -0,0 +1,20 @@
+results = dict();
+
+myOrder  = []
+myOrder.append('30-50')
+myOrder.append('50-80')
+myOrder.append('80-120')
+myOrder.append('120-170')
+myOrder.append('170-300')
+myOrder.append('300-470')
+myOrder.append('470-600')
+myOrder.append('600-800')
+
+results['30-50']   = 'QCD_30to50/LSFJOB_662506132'
+results['50-80']   = 'QCD_50to80/LSFJOB_662506154'
+results['80-120']  = 'QCD_80to120/LSFJOB_662506165'
+results['120-170'] = 'QCD_120to170/LSFJOB_662506187'
+results['170-300'] = 'QCD_170to300/LSFJOB_662506193'
+results['300-470'] = 'QCD_300to470/LSFJOB_662506195'
+results['470-600'] = 'QCD_470to600/LSFJOB_662506212'
+results['600-800'] = 'QCD_600to800/LSFJOB_662506214'
