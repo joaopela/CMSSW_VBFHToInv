68c1e2fa24c61c453e87712f77ff1069b10695cd
Joao Pela <pela@cern.ch>
2015-06-03 21:54:18 +0200

    Now retrives all paramenters and makes separated tables. Calculate times and factors

--- Study_RunII_SampleProd_QCD-VBF-MET/test/Unfiltered/extractResults.py
+++ Study_RunII_SampleProd_QCD-VBF-MET/test/Unfiltered/extractResults.py
@@ -2,6 +2,7 @@
 
 import sys
 import time
+import datetime
 from optparse import OptionParser
 
 parser = OptionParser()
@@ -33,8 +34,11 @@ if options.cfgFile:
   headerTiming.append('Sample')
   headerTiming.append('Job Start')
   headerTiming.append('Job End')
-  headerTiming.append('KSI2K')
-  headerTiming.append('KHS06')
+  headerTiming.append('Total (s)')
+  headerTiming.append('Factor KSI2K')
+  headerTiming.append('KSI2K (s)')
+  headerTiming.append('Factor KHS06')
+  headerTiming.append('KHS06 (s)')
   
   headerStep1 = []
   headerStep1.append('Sample')
@@ -79,9 +83,21 @@ if options.cfgFile:
       
       for line in fIn:
         
-        if line.find('CERN job starter') != -1:
-          strSplit = line.rsplit(': ',1)[1]
-          jobStartTime = strSplit.split(' $',1)[0]
+        
+        
+        # Getting job start time
+        if line.find('Job start time: ') != -1:
+          strSplit = filter(None,re.split(':|\s',line.rsplit('Job start time: ',1)[1]))
+          timeStr  = strSplit[0]+' '+strSplit[1]+' '+strSplit[2].zfill(2)+' '+strSplit[3]+':'+strSplit[4]+':'+strSplit[5]+' '+strSplit[6]+' '+strSplit[7]
+          jobStartTime = timeStr.replace('CEST','CEST')
+          sTime        = datetime.datetime.strptime(jobStartTime,'%a %b %d %H:%M:%S %Z %Y')
+             
+        # Getting job end time
+        if line.find('Job end time: ') != -1:
+          strSplit = filter(None,re.split(':|\s',line.rsplit('Job end time: ',1)[1]))
+          timeStr  = strSplit[0]+' '+strSplit[1]+' '+strSplit[2].zfill(2)+' '+strSplit[3]+':'+strSplit[4]+':'+strSplit[5]+' '+strSplit[6]+' '+strSplit[7]
+          jobStartTime = timeStr.replace('CEST','CEST')
+          eTime        = datetime.datetime.strptime(jobStartTime,'%a %b %d %H:%M:%S %Z %Y')
           
         if line.find('Working directory is') != -1:
           strSplit  = line.rsplit('/',1)[1]
@@ -100,10 +116,6 @@ if options.cfgFile:
         
         if line.find('Before Filtrer: total cross section') != -1:
           totalXSec = line.rsplit('= ',1)[1]
-     
-        if line.find('Job finished at') != -1:
-          strSplit   = line.rsplit('Job finished at ',1)[1]
-          jobEndTime = strSplit.split(' CEST ',1)[0]
           
         if line.find('KSI2K seconds') != -1:
           strSplit  = line.rsplit(' (',1)[1]
@@ -145,8 +157,8 @@ if options.cfgFile:
     print "RAM total    =",mem
     
     
-    print "jobStartTime =",jobStartTime
-    print "jobEndTime   =",jobEndTime
+    print "jobStartTime =",sTime
+    print "jobEndTime   =",eTime
     print "timeKSI2K    =",timeKSI2K
     print "timeKHS06    =",timeKHS06
     print "totalXSec    =",totalXSec
@@ -159,18 +171,23 @@ if options.cfgFile:
     iData.append(mem)
     iData.append(totalXSec)
     
-    #2010/06/23 14:22:16 & Mon May 18 16:34:34
-    sTime = time.strptime(jobStartTime,'%Y/%m/%d %H:%M:%S')
-    eTime = time.strptime(jobEndTime,  '%a %b %d %H:%M:%S')
+    print sTime
+    print eTime
     
-    print sTime[0],sTime[1],sTime[2]," ",sTime[3],sTime[4],sTime[5]
-    print eTime[0],eTime[1],eTime[2]," ",eTime[3],eTime[4],eTime[5]
+    # Time Calculationa
+    diffTime    = eTime-sTime
+    diffTimeSec = diffTime.total_seconds()
+    factorKSI2K = float(timeKSI2K)/diffTimeSec
+    factorKHS06 = float(timeKHS06)/diffTimeSec
     
     iDataTimming = []
     iDataTimming.append(key)
-    iDataTimming.append(jobStartTime)
-    iDataTimming.append(jobEndTime)
+    iDataTimming.append(sTime)
+    iDataTimming.append(eTime)
+    iDataTimming.append(diffTimeSec)
+    iDataTimming.append(factorKSI2K)
     iDataTimming.append(timeKSI2K)
+    iDataTimming.append(factorKHS06)
     iDataTimming.append(timeKHS06)
     
     dataJob    .append(iData)
@@ -182,9 +199,7 @@ if options.cfgFile:
   
   print tabulate(dataJob,headerJob,tablefmt="latex")
   print
-  print tabulate(dataTimming,headerJob,tablefmt="latex")
+  print tabulate(dataTimming,headerTiming,tablefmt="latex")
   print
   print tabulate(dataStep1,headerStep1,tablefmt="latex")
-  print
-  print tabulate(dataStep2,headerStep2,tablefmt="latex")
   
