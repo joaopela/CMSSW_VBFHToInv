a590978f780fd6db8b51d3249e15fecde62d999e
Joao Pela <pela@cern.ch>
2015-06-03 21:53:24 +0200

    Fixing naming problems

--- Study_RunII_SampleProd_QCD-VBF-MET/test/Unfiltered/results_step1_cfg.py
+++ Study_RunII_SampleProd_QCD-VBF-MET/test/Unfiltered/results_step1_cfg.py
@@ -10,11 +10,11 @@ myOrder.append('300-470')
 myOrder.append('470-600')
 myOrder.append('600-800')
 
-results['30-50']   = 'QCD_30to50/LSFJOB_662506132'
-results['50-80']   = 'QCD_50to80/LSFJOB_662506154'
-results['80-120']  = 'QCD_80to120/LSFJOB_662506165'
-results['120-170'] = 'QCD_120to170/LSFJOB_662506187'
-results['170-300'] = 'QCD_170to300/LSFJOB_662506193'
-results['300-470'] = 'QCD_300to470/LSFJOB_662506195'
-results['470-600'] = 'QCD_470to600/LSFJOB_662506212'
-results['600-800'] = 'QCD_600to800/LSFJOB_662506214'
+results['30-50']   = 'QCD_Pt-30to50_step1/LSFJOB_662506132'
+results['50-80']   = 'QCD_Pt-50to80_step1/LSFJOB_662506154'
+results['80-120']  = 'QCD_Pt-80to120_step1/LSFJOB_662506165'
+results['120-170'] = 'QCD_Pt-120to170_step1/LSFJOB_662506187'
+results['170-300'] = 'QCD_Pt-170to300_step1/LSFJOB_662506193'
+results['300-470'] = 'QCD_Pt-300to470_step1/LSFJOB_662506195'
+results['470-600'] = 'QCD_Pt-470to600_step1/LSFJOB_662506212'
+results['600-800'] = 'QCD_Pt-600to800_step1/LSFJOB_662506214'
