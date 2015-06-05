058fa6b96a1a3b58cd3a82ea2dddaa9aa790232b
Joao Pela <pela@cern.ch>
2015-06-05 13:43:44 +0200

    Adding scripts to get results out for step 2

---
+++ Study_RunII_SampleProd_QCD-VBF-MET/test/Unfiltered/results_step2_cfg.py
@@ -0,0 +1,19 @@
+myOrder  = []
+myOrder.append('30-50')
+myOrder.append('50-80')
+myOrder.append('80-120')
+myOrder.append('120-170')
+myOrder.append('170-300')
+myOrder.append('300-470')
+myOrder.append('470-600')
+myOrder.append('600-800')
+
+results = dict();
+results['30-50']   = 'QCD_Pt-30to50_step1/LSFJOB_662506132'
+results['50-80']   = 'QCD_Pt-50to80_step1/LSFJOB_662506154'
+results['80-120']  = 'QCD_Pt-80to120_step1/LSFJOB_662506165'
+results['120-170'] = 'QCD_Pt-120to170_step1/LSFJOB_662506187'
+results['170-300'] = 'QCD_Pt-170to300_step1/LSFJOB_662506193'
+results['300-470'] = 'QCD_Pt-300to470_step1/LSFJOB_662506195'
+results['470-600'] = 'QCD_Pt-470to600_step1/LSFJOB_662506212'
+results['600-800'] = 'QCD_Pt-600to800_step1/LSFJOB_662506214'
