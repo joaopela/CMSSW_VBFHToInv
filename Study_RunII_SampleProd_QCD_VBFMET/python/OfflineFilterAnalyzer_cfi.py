5813f6c25c33414af0ec0b12972a17c5bb9e10f5
Joao Pela <pela@cern.ch>
2015-06-08 01:05:11 +0200

    Selecting ak4 jets

--- Study_RunII_SampleProd_QCD_VBFMET/python/OfflineFilterAnalyzer_cfi.py
+++ Study_RunII_SampleProd_QCD_VBFMET/python/OfflineFilterAnalyzer_cfi.py
@@ -4,7 +4,7 @@ offlineFilterAnalyzer = cms.EDAnalyzer('OfflineFilterAnalyzer',
 
   verbose                   = cms.untracked.bool(False),
   inputTag_HepMCProduct     = cms.untracked.InputTag("generator"),
-  inputTag_GenJetCollection = cms.untracked.InputTag("ak5GenJetsNoNu"),
+  inputTag_GenJetCollection = cms.untracked.InputTag("ak4GenJetsNoNu"),
   inputTag_L1Extra_mets     = cms.untracked.InputTag("hltL1extraParticles","MET"),
   inputTag_HLTResults       = cms.untracked.InputTag("TriggerResults::HLT"),
   outputFilename            = cms.untracked.string('OfflineFilterAnalyzer_Results.root'),
