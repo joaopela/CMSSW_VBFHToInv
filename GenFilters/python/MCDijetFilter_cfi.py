a5e1772d3e6a6effc47ed916481b246b33f2aa6c
Joao Pela <pela@cern.ch>
2015-06-08 01:04:11 +0200

    Updating values for ak4 jets

--- GenFilters/python/MCDijetFilter_cfi.py
+++ GenFilters/python/MCDijetFilter_cfi.py
@@ -14,10 +14,10 @@ mcDijetFilter_Pt20_Eta5p0_DEta3p2_Mjj700 = cms.EDFilter("MCDijetFilter",
 )
 
 # Tight cuts
-mcDijetFilter_Pt50_Eta4p75_DEta3p5_Dphi1p5_Mjj1000 = cms.EDFilter("MCDijetFilter",
+mcDijetFilter_Pt50_Eta4p8_DEta3p5_Dphi1p5_Mjj1000 = cms.EDFilter("MCDijetFilter",
   MinPt = cms.untracked.double(50.0),
-  MaxEta = cms.untracked.double(4.75),
-  MinEta = cms.untracked.double(-4.75),
+  MaxEta = cms.untracked.double(4.8),
+  MinEta = cms.untracked.double(-4.8),
   MinDeltaEta = cms.untracked.double(3.5),
   MaxDeltaEta = cms.untracked.double(10.0),
   MinDeltaPhi = cms.untracked.double(0),
@@ -28,10 +28,10 @@ mcDijetFilter_Pt50_Eta4p75_DEta3p5_Dphi1p5_Mjj1000 = cms.EDFilter("MCDijetFilter
 )
 
 # Same as 'Tight' but with DPhi cut relaxed to 2.0
-mcDijetFilter_Pt50_Eta4p75_DEta3p5_Dphi2p0_Mjj1000 = cms.EDFilter("MCDijetFilter",
+mcDijetFilter_Pt50_Eta4p8_DEta3p5_Dphi2p0_Mjj1000 = cms.EDFilter("MCDijetFilter",
   MinPt = cms.untracked.double(50.0),
-  MaxEta = cms.untracked.double(4.75),
-  MinEta = cms.untracked.double(-4.75),
+  MaxEta = cms.untracked.double(4.8),
+  MinEta = cms.untracked.double(-4.8),
   MinDeltaEta = cms.untracked.double(3.5),
   MaxDeltaEta = cms.untracked.double(10.0),
   MinDeltaPhi = cms.untracked.double(0),
@@ -42,10 +42,10 @@ mcDijetFilter_Pt50_Eta4p75_DEta3p5_Dphi2p0_Mjj1000 = cms.EDFilter("MCDijetFilter
 )
 
 # Same as 'Tight' but with DPhi cut relaxed to 2.5
-mcDijetFilter_Pt50_Eta4p75_DEta3p5_Dphi2p5_Mjj1000 = cms.EDFilter("MCDijetFilter",
+mcDijetFilter_Pt50_Eta4p8_DEta3p5_Dphi2p5_Mjj1000 = cms.EDFilter("MCDijetFilter",
   MinPt = cms.untracked.double(50.0),
-  MaxEta = cms.untracked.double(4.75),
-  MinEta = cms.untracked.double(-4.75),
+  MaxEta = cms.untracked.double(4.8),
+  MinEta = cms.untracked.double(-4.8),
   MinDeltaEta = cms.untracked.double(3.5),
   MaxDeltaEta = cms.untracked.double(10.0),
   MinDeltaPhi = cms.untracked.double(0),
@@ -56,10 +56,10 @@ mcDijetFilter_Pt50_Eta4p75_DEta3p5_Dphi2p5_Mjj1000 = cms.EDFilter("MCDijetFilter
 )
 
 # Same as 'Tight' but with no DPhi cuts
-mcDijetFilter_Pt50_Eta4p75_DEta3p5_Mjj1000 = cms.EDFilter("MCDijetFilter",
+mcDijetFilter_Pt50_Eta4p8_DEta3p5_Mjj1000 = cms.EDFilter("MCDijetFilter",
   MinPt = cms.untracked.double(50.0),
-  MaxEta = cms.untracked.double(4.75),
-  MinEta = cms.untracked.double(-4.75),
+  MaxEta = cms.untracked.double(4.8),
+  MinEta = cms.untracked.double(-4.8),
   MinDeltaEta = cms.untracked.double(3.5),
   MaxDeltaEta = cms.untracked.double(10.0),
   MinDeltaPhi = cms.untracked.double(0),
@@ -70,10 +70,10 @@ mcDijetFilter_Pt50_Eta4p75_DEta3p5_Mjj1000 = cms.EDFilter("MCDijetFilter",
 )
 
 # Tight cuts but with Pt cut relaxed to 40
-mcDijetFilter_Pt40_Eta4p75_DEta3p5_Dphi1p5_Mjj1000 = cms.EDFilter("MCDijetFilter",
+mcDijetFilter_Pt40_Eta4p8_DEta3p5_Dphi1p5_Mjj1000 = cms.EDFilter("MCDijetFilter",
   MinPt = cms.untracked.double(40.0),
-  MaxEta = cms.untracked.double(4.75),
-  MinEta = cms.untracked.double(-4.75),
+  MaxEta = cms.untracked.double(4.8),
+  MinEta = cms.untracked.double(-4.8),
   MinDeltaEta = cms.untracked.double(3.5),
   MaxDeltaEta = cms.untracked.double(10.0),
   MinDeltaPhi = cms.untracked.double(0),
@@ -86,10 +86,10 @@ mcDijetFilter_Pt40_Eta4p75_DEta3p5_Dphi1p5_Mjj1000 = cms.EDFilter("MCDijetFilter
 #####################################
 # HLT jet cuts
 #####################################
-mcDijetFilter_Pt40_Eta4p75_DEta3p5_Mjj600 = cms.EDFilter("MCDijetFilter",
+mcDijetFilter_Pt40_Eta4p8_DEta3p5_Mjj600 = cms.EDFilter("MCDijetFilter",
   MinPt = cms.untracked.double(40.0),
-  MaxEta = cms.untracked.double(4.75),
-  MinEta = cms.untracked.double(-4.75),
+  MaxEta = cms.untracked.double(4.8),
+  MinEta = cms.untracked.double(-4.8),
   MinDeltaEta = cms.untracked.double(3.5),
   MaxDeltaEta = cms.untracked.double(10.0),
   MinDeltaPhi = cms.untracked.double(0),
@@ -100,10 +100,10 @@ mcDijetFilter_Pt40_Eta4p75_DEta3p5_Mjj600 = cms.EDFilter("MCDijetFilter",
 )
 
 # HLT jet cuts + Tight DPhi cuts
-mcDijetFilter_Pt40_Eta4p75_DEta3p5_Dphi1p5_Mjj600 = cms.EDFilter("MCDijetFilter",
+mcDijetFilter_Pt40_Eta4p8_DEta3p5_Dphi1p5_Mjj600 = cms.EDFilter("MCDijetFilter",
   MinPt = cms.untracked.double(40.0),
-  MaxEta = cms.untracked.double(4.75),
-  MinEta = cms.untracked.double(-4.75),
+  MaxEta = cms.untracked.double(4.8),
+  MinEta = cms.untracked.double(-4.8),
   MinDeltaEta = cms.untracked.double(3.5),
   MaxDeltaEta = cms.untracked.double(10.0),
   MinDeltaPhi = cms.untracked.double(0),
@@ -114,7 +114,7 @@ mcDijetFilter_Pt40_Eta4p75_DEta3p5_Dphi1p5_Mjj600 = cms.EDFilter("MCDijetFilter"
 )
 
 # Working Point B
-mcDijetFilter_Pt50_Eta4p75_DEta3p0_Dphi2p0_Mjj1000 = cms.EDFilter("MCDijetFilter",
+mcDijetFilter_Pt50_Eta4p8_DEta3p0_Dphi2p0_Mjj1000 = cms.EDFilter("MCDijetFilter",
   MinPt       = cms.untracked.double(40.0),
   MaxEta      = cms.untracked.double(4.75),
   MinEta      = cms.untracked.double(-4.75),
@@ -131,6 +131,6 @@ mcDijetFilter_Pt50_Eta4p75_DEta3p0_Dphi2p0_Mjj1000 = cms.EDFilter("MCDijetFilter
 
 # Alias
 mcDijetFilter_RunI      = mcDijetFilter_Pt20_Eta5p0_DEta3p2_Mjj700
-mcDijetFilter_RunII_WPA = mcDijetFilter_Pt50_Eta4p75_DEta3p5_Dphi1p5_Mjj1000
-mcDijetFilter_RunII_WPB = mcDijetFilter_Pt50_Eta4p75_DEta3p0_Dphi2p0_Mjj1000
+mcDijetFilter_RunII_WPA = mcDijetFilter_Pt50_Eta4p8_DEta3p5_Dphi1p5_Mjj1000
+mcDijetFilter_RunII_WPB = mcDijetFilter_Pt50_Eta4p8_DEta3p0_Dphi2p0_Mjj1000
 
