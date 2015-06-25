f2686b6952076b2793b1bde3d0e77e8c88323d8c
Joao Pela <pela@cern.ch>
2015-06-25 15:51:05 +0200

    First version#

---
+++ Study_RunII_TriggerStudies/test/hadd_HLTSimpleAnalyzer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# HLTSimpleAnalyser PU20bx25 HCALMethod0
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod0_VBF_HToInv_M-125.root  HLTSimpleAnalyser/PU20bx25/HCALMethod0/crab_VBF_HToInv_M-125_PU20bx25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod0_QCD_Pt-50to80.root     HLTSimpleAnalyser/PU20bx25/HCALMethod0/crab_QCD_Pt-50to80_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod0_QCD_Pt-80to120.root    HLTSimpleAnalyser/PU20bx25/HCALMethod0/crab_QCD_Pt-80to120_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod0_QCD_Pt-120to170.root   HLTSimpleAnalyser/PU20bx25/HCALMethod0/crab_QCD_Pt-120to170_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod0_QCD_Pt-170to300.root   HLTSimpleAnalyser/PU20bx25/HCALMethod0/crab_QCD_Pt-170to300_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod0_QCD_Pt-300to470.root   HLTSimpleAnalyser/PU20bx25/HCALMethod0/crab_QCD_Pt-300to470_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod0_QCD_Pt-470to600.root   HLTSimpleAnalyser/PU20bx25/HCALMethod0/crab_QCD_Pt-470to600_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod0_QCD_Pt-600to800.root   HLTSimpleAnalyser/PU20bx25/HCALMethod0/crab_QCD_Pt-600to800_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod0_QCD_Pt-1000to1400.root HLTSimpleAnalyser/PU20bx25/HCALMethod0/crab_QCD_Pt-1000to1400_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod0_QCD_Pt-1400to1800.root HLTSimpleAnalyser/PU20bx25/HCALMethod0/crab_QCD_Pt-1400to1800_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod0_QCD_Pt-1800.root       HLTSimpleAnalyser/PU20bx25/HCALMethod0/crab_QCD_Pt-1800_AVE20BX25/results/*.root
+
+# HLTSimpleAnalyser PU20bx25 HCALMethod3
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod3_VBF_HToInv_M-125.root  HLTSimpleAnalyser/PU20bx25/HCALMethod3/crab_VBF_HToInv_M-125_PU20bx25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod3_QCD_Pt-50to80.root     HLTSimpleAnalyser/PU20bx25/HCALMethod3/crab_QCD_Pt-50to80_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod3_QCD_Pt-80to120.root    HLTSimpleAnalyser/PU20bx25/HCALMethod3/crab_QCD_Pt-80to120_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod3_QCD_Pt-120to170.root   HLTSimpleAnalyser/PU20bx25/HCALMethod3/crab_QCD_Pt-120to170_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod3_QCD_Pt-170to300.root   HLTSimpleAnalyser/PU20bx25/HCALMethod3/crab_QCD_Pt-170to300_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod3_QCD_Pt-300to470.root   HLTSimpleAnalyser/PU20bx25/HCALMethod3/crab_QCD_Pt-300to470_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod3_QCD_Pt-470to600.root   HLTSimpleAnalyser/PU20bx25/HCALMethod3/crab_QCD_Pt-470to600_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod3_QCD_Pt-600to800.root   HLTSimpleAnalyser/PU20bx25/HCALMethod3/crab_QCD_Pt-600to800_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod3_QCD_Pt-1000to1400.root HLTSimpleAnalyser/PU20bx25/HCALMethod3/crab_QCD_Pt-1000to1400_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod3_QCD_Pt-1400to1800.root HLTSimpleAnalyser/PU20bx25/HCALMethod3/crab_QCD_Pt-1400to1800_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod3_QCD_Pt-1800.root       HLTSimpleAnalyser/PU20bx25/HCALMethod3/crab_QCD_Pt-1800_AVE20BX25/results/*.root
