08904a777fe0a889422ca027224f4f40aa21b84b
Joao Pela <pela@cern.ch>
2015-06-29 13:53:19 +0200

    Adding missing pT hat (800to1000)

--- Study_RunII_TriggerStudies/test/hadd_HLTSimpleAnalyzer.sh
+++ Study_RunII_TriggerStudies/test/hadd_HLTSimpleAnalyzer.sh
@@ -9,6 +9,7 @@ hadd HLTSimpleAnalyser_PU20bx25_HCALMethod0_QCD_Pt-170to300.root   HLTSimpleAnal
 hadd HLTSimpleAnalyser_PU20bx25_HCALMethod0_QCD_Pt-300to470.root   HLTSimpleAnalyser/PU20bx25/HCALMethod0/crab_QCD_Pt-300to470_AVE20BX25/results/*.root
 hadd HLTSimpleAnalyser_PU20bx25_HCALMethod0_QCD_Pt-470to600.root   HLTSimpleAnalyser/PU20bx25/HCALMethod0/crab_QCD_Pt-470to600_AVE20BX25/results/*.root
 hadd HLTSimpleAnalyser_PU20bx25_HCALMethod0_QCD_Pt-600to800.root   HLTSimpleAnalyser/PU20bx25/HCALMethod0/crab_QCD_Pt-600to800_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod0_QCD_Pt-800to1000.root  HLTSimpleAnalyser/PU20bx25/HCALMethod3/crab_QCD_Pt-800to1000_AVE20BX25/results/*.root
 hadd HLTSimpleAnalyser_PU20bx25_HCALMethod0_QCD_Pt-1000to1400.root HLTSimpleAnalyser/PU20bx25/HCALMethod0/crab_QCD_Pt-1000to1400_AVE20BX25/results/*.root
 hadd HLTSimpleAnalyser_PU20bx25_HCALMethod0_QCD_Pt-1400to1800.root HLTSimpleAnalyser/PU20bx25/HCALMethod0/crab_QCD_Pt-1400to1800_AVE20BX25/results/*.root
 hadd HLTSimpleAnalyser_PU20bx25_HCALMethod0_QCD_Pt-1800.root       HLTSimpleAnalyser/PU20bx25/HCALMethod0/crab_QCD_Pt-1800_AVE20BX25/results/*.root
@@ -22,6 +23,7 @@ hadd HLTSimpleAnalyser_PU20bx25_HCALMethod3_QCD_Pt-170to300.root   HLTSimpleAnal
 hadd HLTSimpleAnalyser_PU20bx25_HCALMethod3_QCD_Pt-300to470.root   HLTSimpleAnalyser/PU20bx25/HCALMethod3/crab_QCD_Pt-300to470_AVE20BX25/results/*.root
 hadd HLTSimpleAnalyser_PU20bx25_HCALMethod3_QCD_Pt-470to600.root   HLTSimpleAnalyser/PU20bx25/HCALMethod3/crab_QCD_Pt-470to600_AVE20BX25/results/*.root
 hadd HLTSimpleAnalyser_PU20bx25_HCALMethod3_QCD_Pt-600to800.root   HLTSimpleAnalyser/PU20bx25/HCALMethod3/crab_QCD_Pt-600to800_AVE20BX25/results/*.root
+hadd HLTSimpleAnalyser_PU20bx25_HCALMethod3_QCD_Pt-800to1000.root  HLTSimpleAnalyser/PU20bx25/HCALMethod3/crab_QCD_Pt-800to1000_AVE20BX25/results/*.root
 hadd HLTSimpleAnalyser_PU20bx25_HCALMethod3_QCD_Pt-1000to1400.root HLTSimpleAnalyser/PU20bx25/HCALMethod3/crab_QCD_Pt-1000to1400_AVE20BX25/results/*.root
 hadd HLTSimpleAnalyser_PU20bx25_HCALMethod3_QCD_Pt-1400to1800.root HLTSimpleAnalyser/PU20bx25/HCALMethod3/crab_QCD_Pt-1400to1800_AVE20BX25/results/*.root
 hadd HLTSimpleAnalyser_PU20bx25_HCALMethod3_QCD_Pt-1800.root       HLTSimpleAnalyser/PU20bx25/HCALMethod3/crab_QCD_Pt-1800_AVE20BX25/results/*.root
