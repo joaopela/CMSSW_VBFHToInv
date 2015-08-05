276d1fcafa7194db88203adb466cd67a902c08ef
Joao Pela <pela@cern.ch>
2015-08-05 18:58:39 +0200

    Splitting into 2 sub jobs

--- Study_RunII_SampleProd_QCD_VBFMET/test/MadGraph/CMSSW_7_1_18/run_GEN_overLHE_cfg.py
+++ Study_RunII_SampleProd_QCD_VBFMET/test/MadGraph/CMSSW_7_1_18/run_GEN_overLHE_cfg.py
@@ -3,6 +3,23 @@
 # Revision: 1.19 
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
 # with command line options: Configuration/Generator/python/Hadronizer_TuneCUETP8M1_13TeV_MLM_5f_max4j_LHE_pythia8_cff.py --filein file:unweighted_events.lhe --fileout file:lheSIM.root --mc --eventcontent RAWSIM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/StandardSequences/SimWithCastor_cff.customise --datatier GEN-SIM --conditions MCRUN2_71_V1::All --step GEN,SIM --magField 38T_PostLS1 --geometry Extended2015 --python_filename processLHE_cfg.py --no_exec -n 10
+
+import FWCore.ParameterSet.VarParsing as VarParsing
+
+# setup 'analysis'  options
+options = VarParsing.VarParsing ('analysis')
+
+# setup any defaults you want
+options.inputFiles = 'mg5_pp_jj_etaj4p8_ptj1min30_ptj2min30_mmjj800_unweighted_events.lhe'
+options.outputFile = 'CMSSW_7_1_18_Hadronizer_pp_jj_etaj4p8_ptj1min30_ptj2min30_mmjj800_unweighted_events.root'
+options.maxEvents  = -1
+
+# get and parse the command line arguments
+options.parseArguments()
+
+#print os.path.splitext(inputLHE)[0]
+#sys.exit()
+
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('SIM')
@@ -27,12 +44,12 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(options.maxEvents)
 )
 
 # Input source
 process.source = cms.Source("LHESource",
-    fileNames = cms.untracked.vstring('file:unweighted_events.lhe'),
+    fileNames = cms.untracked.vstring(options.inputFiles),
     inputCommands = cms.untracked.vstring('keep *', 
         'drop LHEXMLStringProduct_*_*_*'),
     dropDescendantsOfDroppedBranches = cms.untracked.bool(False)
@@ -55,7 +72,7 @@ process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
     splitLevel = cms.untracked.int32(0),
     eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
     outputCommands = process.RAWSIMEventContent.outputCommands,
-    fileName = cms.untracked.string('file:lheSIM.root'),
+    fileName = cms.untracked.string(options.outputFile),
     dataset = cms.untracked.PSet(
         filterName = cms.untracked.string(''),
         dataTier = cms.untracked.string('GEN-SIM')
@@ -113,22 +130,23 @@ process.generator = cms.EDFilter("Pythia8HadronizerFilter",
 
 process.ProductionFilterSequence = cms.Sequence(process.generator)
 
-###############################################################
-# START: HLT Simple Analysis
-###############################################################
-process.analyzer = cms.EDAnalyzer('PartonGenJetAnalyzer',
-
-  outputFilename = cms.untracked.string("PartonGenJetAnalyzer_Results.root"),
-
-)
-
 # Path and EndPath definitions
 process.generation_step = cms.Path(
   process.pgen*
   process.genParticlesForJetsNoNu*
-  process.ak4GenJetsNoNu*
-  process.analyzer
+  process.ak4GenJetsNoNu
 )
+process.generation_step.remove(process.ak4GenJets)
+process.generation_step.remove(process.ak5GenJets)
+process.generation_step.remove(process.ak8GenJets)
+process.generation_step.remove(process.iterativeCone5GenJets)
+process.generation_step.remove(process.genMetIC5GenJets)
+process.generation_step.remove(process.kt4GenJets)
+process.generation_step.remove(process.kt6GenJets)
+process.generation_step.remove(process.genMetCalo)
+process.generation_step.remove(process.genMetCaloAndNonPrompt)
+process.generation_step.remove(process.genMetTrue)
+
 process.simulation_step = cms.Path(process.psim)
 process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
 process.endjob_step = cms.EndPath(process.endOfProcess)
