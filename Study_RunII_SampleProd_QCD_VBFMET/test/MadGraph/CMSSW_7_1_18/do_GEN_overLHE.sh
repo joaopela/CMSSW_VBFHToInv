42b050907588f5696932b83c685e971641c308b7
Joao Pela <pela@cern.ch>
2015-08-29 04:29:21 +0200

    Adding new jobs

--- Study_RunII_SampleProd_QCD_VBFMET/test/MadGraph/CMSSW_7_1_18/do_GEN_overLHE.sh
+++ Study_RunII_SampleProd_QCD_VBFMET/test/MadGraph/CMSSW_7_1_18/do_GEN_overLHE.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-cmsRun run_GEN_overLHE_cfg.py print inputFiles=file:mg5_pp_jj_etaj4p8_ptj1min30_ptj2min30_mmjj800_unweighted_events.lhe outputFile=file:CMSSW_7_1_18_Hadronizer_mg5_pp_jj_etaj4p8_ptj1min30_ptj2min30_mmjj800_unweighted_events.root maxEvents=-1
+# cmsRun run_GEN_overLHE_cfg.py print inputFiles=file:mg5_pp_jj_etaj4p8_ptj1min30_ptj2min30_mmjj800_unweighted_events.lhe outputFile=file:CMSSW_7_1_18_Hadronizer_mg5_pp_jj_etaj4p8_ptj1min30_ptj2min30_mmjj800_unweighted_events.root maxEvents=-1
+
+# Run over version 1 files
+cmsRun run_GEN_overLHE_cfg.py print inputFiles=file:v1/QCD_VBFLike_jj_unweighted_events.lhe  outputFile=file:v1/CMSSW_7_1_18_Hadronizer_QCD_VBFLike_jj.root maxEvents=-1
+cmsRun run_GEN_overLHE_cfg.py print inputFiles=file:v1/QCD_VBFLike_jjj_unweighted_events.lhe outputFile=file:v1/CMSSW_7_1_18_Hadronizer_QCD_VBFLike_jjj.root maxEvents=-1
