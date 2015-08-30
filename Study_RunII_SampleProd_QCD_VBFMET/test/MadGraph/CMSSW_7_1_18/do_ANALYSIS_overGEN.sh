5d3014bafbbad4b5ab1cd758da6124dbac4f3a2d
Joao Pela <pela@cern.ch>
2015-08-30 03:03:34 +0200

    Adding more jobs

--- Study_RunII_SampleProd_QCD_VBFMET/test/MadGraph/CMSSW_7_1_18/do_ANALYSIS_overGEN.sh
+++ Study_RunII_SampleProd_QCD_VBFMET/test/MadGraph/CMSSW_7_1_18/do_ANALYSIS_overGEN.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cmsRun run_ANALYSIS_overGEN_cfg.py print inputFiles=file:lheSIM.root outputFile=file:PartonGenJetAnalyzer_Results_mg5_pp_jj_etaj4p8_ptj1min30_ptj2min30_mmjj800_unweighted_events.root maxEvents=-1
+# cmsRun run_ANALYSIS_overGEN_cfg.py print inputFiles=file:lheSIM.root outputFile=file:PartonGenJetAnalyzer_Results_mg5_pp_jj_etaj4p8_ptj1min30_ptj2min30_mmjj800_unweighted_events.root maxEvents=-1
 
 # Run over version 1 files
 cmsRun run_ANALYSIS_overGEN_cfg.py print inputFiles=file:v1/CMSSW_7_1_18_Hadronizer_QCD_VBFLike_jj.root   outputFile=file:v1/PartonGenJetAnalyzer_QCD_VBFLike_jj.root   maxEvents=-1
