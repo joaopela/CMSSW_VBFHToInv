c48448efe5d2387c1db6cd176f984a7a6f0de093
Joao Pela <pela@cern.ch>
2015-11-11 20:01:32 +0100

    Setting oppositeHemisphere to false on all modules

--- GenFilters/python/VBFGenJetFilter_cfi.py
+++ GenFilters/python/VBFGenJetFilter_cfi.py
@@ -4,15 +4,16 @@ vbfGenJetFilterA = cms.EDFilter("VBFGenJetFilter",
 
   inputTag_GenJetCollection = cms.untracked.InputTag('ak4GenJetsNoNu'),
 
-  minPt       = cms.untracked.double(    40),
-  minEta      = cms.untracked.double(  -4.8),
-  maxEta      = cms.untracked.double(   4.8),
-  minInvMass  = cms.untracked.double( 1000.),
-  maxInvMass  = cms.untracked.double(99999.),
-  minDeltaPhi = cms.untracked.double(  -1.0),
-  maxDeltaPhi = cms.untracked.double(  2.15),
-  minDeltaEta = cms.untracked.double(   3.0),
-  maxDeltaEta = cms.untracked.double(99999.)
+  oppositeHemisphere = cms.untracked.bool  ( False), # Require j1_eta*j2_eta<0
+  minPt              = cms.untracked.double(    40), # Minimum dijet jet_pt
+  minEta             = cms.untracked.double(  -4.8), # Minimum dijet jet_eta
+  maxEta             = cms.untracked.double(   4.8), # Maximum dijet jet_eta
+  minInvMass         = cms.untracked.double( 1000.), # Minimum dijet invariant mass
+  maxInvMass         = cms.untracked.double(99999.), # Maximum dijet invariant mass
+  minDeltaPhi        = cms.untracked.double(  -1.0), # Minimum dijet delta phi
+  maxDeltaPhi        = cms.untracked.double(  2.15), # Maximum dijet delta phi
+  minDeltaEta        = cms.untracked.double(   3.0), # Minimum dijet delta eta
+  maxDeltaEta        = cms.untracked.double(99999.)  # Maximum dijet delta eta
 
 )
 
@@ -20,15 +21,16 @@ vbfGenJetFilterB = cms.EDFilter("VBFGenJetFilter",
 
   inputTag_GenJetCollection = cms.untracked.InputTag('ak4GenJetsNoNu'),
 
-  minPt       = cms.untracked.double(    40),
-  minEta      = cms.untracked.double(  -4.8),
-  maxEta      = cms.untracked.double(   4.8),
-  minInvMass  = cms.untracked.double( 1000.),
-  maxInvMass  = cms.untracked.double(99999.),
-  minDeltaPhi = cms.untracked.double(  2.15),
-  maxDeltaPhi = cms.untracked.double(   3.2),
-  minDeltaEta = cms.untracked.double(   3.0),
-  maxDeltaEta = cms.untracked.double(99999.)
+  oppositeHemisphere = cms.untracked.bool  ( False), # Require j1_eta*j2_eta<0
+  minPt              = cms.untracked.double(    40), # Minimum dijet jet_pt
+  minEta             = cms.untracked.double(  -4.8), # Minimum dijet jet_eta
+  maxEta             = cms.untracked.double(   4.8), # Maximum dijet jet_eta
+  minInvMass         = cms.untracked.double( 1000.), # Minimum dijet invariant mass
+  maxInvMass         = cms.untracked.double(99999.), # Maximum dijet invariant mass
+  minDeltaPhi        = cms.untracked.double(  2.15), # Minimum dijet delta phi
+  maxDeltaPhi        = cms.untracked.double(   3.2), # Maximum dijet delta phi
+  minDeltaEta        = cms.untracked.double(   3.0), # Minimum dijet delta eta
+  maxDeltaEta        = cms.untracked.double(99999.)  # Maximum dijet delta eta
 
 )
 
@@ -36,14 +38,15 @@ vbfGenJetFilterC = cms.EDFilter("VBFGenJetFilter",
 
   inputTag_GenJetCollection = cms.untracked.InputTag('ak4GenJetsNoNu'),
 
-  minPt       = cms.untracked.double(    40),
-  minEta      = cms.untracked.double(  -4.8),
-  maxEta      = cms.untracked.double(   4.8),
-  minInvMass  = cms.untracked.double( 1000.),
-  maxInvMass  = cms.untracked.double(99999.),
-  minDeltaPhi = cms.untracked.double(  -1.0),
-  maxDeltaPhi = cms.untracked.double(   3.2),
-  minDeltaEta = cms.untracked.double(   3.0),
-  maxDeltaEta = cms.untracked.double(99999.)
+  oppositeHemisphere = cms.untracked.bool  ( False), # Require j1_eta*j2_eta<0
+  minPt              = cms.untracked.double(    40), # Minimum dijet jet_pt
+  minEta             = cms.untracked.double(  -4.8), # Minimum dijet jet_eta
+  maxEta             = cms.untracked.double(   4.8), # Maximum dijet jet_eta
+  minInvMass         = cms.untracked.double( 1000.), # Minimum dijet invariant mass
+  maxInvMass         = cms.untracked.double(99999.), # Maximum dijet invariant mass
+  minDeltaPhi        = cms.untracked.double(  -1.0), # Minimum dijet delta phi
+  maxDeltaPhi        = cms.untracked.double(   3.2), # Maximum dijet delta phi
+  minDeltaEta        = cms.untracked.double(   3.0), # Minimum dijet delta eta
+  maxDeltaEta        = cms.untracked.double(99999.)  # Maximum dijet delta eta
 
 )
