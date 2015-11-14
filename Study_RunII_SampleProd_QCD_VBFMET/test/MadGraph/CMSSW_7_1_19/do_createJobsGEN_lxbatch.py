b0e6f9740cfa2c56c8b2fc287a7c2d84154527d0
Joao Pela <pela@cern.ch>
2015-11-14 17:30:08 +0100

    Changing gridpack

--- Study_RunII_SampleProd_QCD_VBFMET/test/MadGraph/CMSSW_7_1_19/do_createJobsGEN_lxbatch.py
+++ Study_RunII_SampleProd_QCD_VBFMET/test/MadGraph/CMSSW_7_1_19/do_createJobsGEN_lxbatch.py
@@ -30,10 +30,10 @@ for x in range(sJobs,nJobs):
     fOut.write("cd CMSSW_7_1_19/src\n")
     fOut.write("eval `scramv1 runtime -sh`\n")
     fOut.write("\n")
-    fOut.write("tar -xvaf /afs/cern.ch/work/p/pela/public/qcd_vbf_samples/gridpack_v3/qcd_vbf_tarball.tar.xz\n")
+    fOut.write("tar -xvaf /afs/cern.ch/work/p/pela/public/qcd_vbf_samples/gridpack_v2/qcd_vbf_tarball.tar.xz\n")
     fOut.write("./runcmsgrid.sh $EVENTS $JOB 1\n")
     fOut.write("\n")
-    fOut.write("mv cmsgrid_final.lhe /afs/cern.ch/work/p/pela/public/qcd_vbf_samples/gridpack_v3/events/ev_seed${JOB}_n${EVENTS}.lhe\n")
+    fOut.write("mv cmsgrid_final.lhe /afs/cern.ch/work/p/pela/public/qcd_vbf_samples/gridpack_v2/events/ev_seed${JOB}_n${EVENTS}.lhe\n")
 
     os.chmod(path, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
 
